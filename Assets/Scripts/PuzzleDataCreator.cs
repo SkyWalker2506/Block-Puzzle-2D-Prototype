46de3392c624adf30494e717d1a16299aa6fbc38
SkyWalker2506 <42008533+SkyWalker2506@users.noreply.github.com>
2021-01-16 14:26:04 +0300

    Update

---
+++ Assets/Scripts/PuzzleDataCreator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Puzzle2DSystem
+{
+    public class PuzzleDataCreator 
+    {
+        int minBoardSize;
+        int maxBoardSize;
+
+        public PuzzleDataCreator (int minBoardSize=4, int maxBoardSize=6)
+        {
+            this.minBoardSize = minBoardSize;
+            this.maxBoardSize = maxBoardSize;
+        }
+
+
+
+    }
+
+    [System.Serializable]
+    public class PuzzlePiece
+    {
+        public Vector2[] PieceData { get; }
+
+        public PuzzlePiece(Vector2[] pieceData)
+        {
+            PieceData = pieceData;
+        }
+
+    }
+
+}
