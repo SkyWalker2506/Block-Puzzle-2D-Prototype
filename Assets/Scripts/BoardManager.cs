edcf8989bda8106a620d21e7d150aa3e6ce7c95f
SkyWalker2506 <42008533+SkyWalker2506@users.noreply.github.com>
2021-01-18 03:36:53 +0300

    Update

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -5,17 +5,75 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    int minBoardSize=4;
-    int maxBoardSize=6;
-    int minPuzzlePieceCount;
-    int maxPuzzlePieceCount;
+    [SerializeField] int minBoardSize=4;
+    [SerializeField] int maxBoardSize = 6;
+    [SerializeField] int minPuzzlePieceCount=5;
+    [SerializeField] int maxPuzzlePieceCount=8;
+    [SerializeField] Material materialPrefab;
 
     PuzzleDataCreator puzzleDataCreator;
+    PuzzleData puzzleData;
 
     private void Awake()
     {
-        puzzleDataCreator = new PuzzleDataCreator();
-        puzzleDataCreator.GetRandomPuzzleData();
+        puzzleDataCreator = new PuzzleDataCreator(minBoardSize, maxBoardSize,minPuzzlePieceCount,maxPuzzlePieceCount);
+        puzzleData = puzzleDataCreator.GetRandomPuzzleData();
+        Camera.main.orthographicSize = puzzleData.PuzzleBoard.BoardSize ;
+        Camera.main.transform.position =  Vector2.one * puzzleData.PuzzleBoard.BoardSize*.5f;
+        Camera.main.transform.position -= Vector3.forward;
+        CreatePuzzlePiecesVisual();
+        CreateBoardVisual();
     }
 
+    void CreateBoardVisual()
+    {
+        print(puzzleData.PuzzlePieces.Count);
+        var board = new GameObject("Board");
+        var renderer = board.AddComponent<LineRenderer>();
+        var material = Instantiate(materialPrefab);
+        renderer.material = material;
+        renderer.startWidth = .2f;
+        renderer.endWidth = .2f;
+        renderer.positionCount = 5;
+        renderer.SetPosition(0, Vector2.zero);
+        renderer.SetPosition(1, puzzleData.PuzzleBoard.BoardSize * Vector2.up);
+        renderer.SetPosition(2, puzzleData.PuzzleBoard.BoardSize * (Vector2.up+ Vector2.right));
+        renderer.SetPosition(3, puzzleData.PuzzleBoard.BoardSize * Vector2.right);
+        renderer.SetPosition(4, Vector2.zero);
+    }
+
+    void CreatePuzzlePiecesVisual()
+    {
+        print(puzzleData.PuzzlePieces.Count);
+        puzzleData.PuzzlePieces.ForEach(CreatePuzzlePiece);
+    }
+
+
+    void CreatePuzzlePiece(PuzzlePiece puzzlePiece)
+    {
+        print(puzzlePiece.TriangleDatas.Length);
+        var piece = new GameObject("piece");
+        var material = Instantiate(materialPrefab);
+        material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
+        for (int i = 0; i < puzzlePiece.TriangleDatas.Length; i++)
+        {
+             CreateTriangle(puzzlePiece.TriangleDatas[i], material).transform.SetParent(piece.transform);
+        }
+    }
+
+    GameObject CreateTriangle(TriangleData triangleData,Material material)
+    {
+        var triangle = new GameObject("puzzlePieceTriangle");
+        var renderer = triangle.AddComponent<LineRenderer>();
+        renderer.material = material;
+        renderer.startWidth= .2f;
+        renderer.endWidth= .2f;
+        renderer.positionCount = 4;
+        for (int i = 0; i < 4; i++)
+        {
+            renderer.SetPosition(i, triangleData.Points[i%3]);
+        }
+        return triangle;
+    }
 }
--- Assets/Scripts/PuzzleDataCreator.cs
+++ Assets/Scripts/PuzzleDataCreator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Puzzle2DSystem
 {
@@ -20,34 +21,101 @@ namespace Puzzle2DSystem
         public PuzzleData GetRandomPuzzleData()
         {
             int boardSize = Random.Range(minBoardSize, maxBoardSize + 1);
-            return new PuzzleData(boardSize);
+            return new PuzzleData(boardSize,maxPuzzlePieceCount);
         }
     }
 
-    [System.Serializable]
+   // [System.Serializable]
     public class PuzzleData
     {
         public PuzzleBoard PuzzleBoard { get; }
-        public PuzzlePiece[] PuzzlePieces { get; }
+        public List<PuzzlePiece> PuzzlePieces { get; private set; }
+        List<TriangleData> availableTriangles; 
+        List<TriangleData> notAvailableNeighbourTriangles;
+        List<TriangleData> usedTriangles;
+        int maxPuzzlePieceCount;
+        int maxTriangleCount;
 
-        public PuzzleData(int boardSize)
+        public PuzzleData(int boardSize, int maxPuzzlePieceCount, int maxTriangleCount = 25)
         {
             PuzzleBoard  = new PuzzleBoard(boardSize);
+            this.maxPuzzlePieceCount = maxPuzzlePieceCount;
+            this.maxTriangleCount = boardSize* boardSize*4;
+            PuzzlePieces = new List<PuzzlePiece>();
+            availableTriangles = new List<TriangleData>();
+            notAvailableNeighbourTriangles = new List<TriangleData>();
+            usedTriangles = new List<TriangleData>();
+            Debug.Log("contactPoints " + PuzzleBoard.ContactPoints.Length);
+            foreach (var contactPoint in PuzzleBoard.ContactPoints)
+            {
+                availableTriangles.AddRange(contactPoint.Triangles);
+            }
+            var triangles = availableTriangles.ToArray();
+            foreach (var triangle in triangles)
+            {
+                triangle.SetMyNeighbours(triangles);
+            }
+
+            for (int i = 0; i < maxPuzzlePieceCount; i++)
+            {
+                CreatePuzzlePiece();
+            }
         }
 
-        void SetPuzzlePieces()
+        void CreatePuzzlePiece()
         {
+            Debug.Log("availableTriangles "+ availableTriangles.Count);
+            if (availableTriangles.Count == 0)
+                return;
 
+            List<TriangleData> triangleDatas = new List<TriangleData>();
+            triangleDatas.Add(availableTriangles[0]);
+            var randomNeighbour = triangleDatas[0].GetRandomAvailableNeighbour();
+            if (randomNeighbour == null)
+                return;
+            triangleDatas.Add(randomNeighbour);
+
+            for (int i = 1; i < maxTriangleCount; i++)
+            {
+                randomNeighbour = triangleDatas[i].GetRandomAvailableNeighbour();
+                if (randomNeighbour == null)
+                    break;
+                triangleDatas.Add(randomNeighbour);
+            }
+
+            var puzzlePiece = new PuzzlePiece(triangleDatas.ToArray());
+            PuzzlePieces.Add(puzzlePiece);
+
+            foreach (var triangleData in triangleDatas)
+            {
+                availableTriangles.Remove(triangleData);
+                usedTriangles.Add(triangleData);
+            }
+            SetNoAvailableNeighbourTriangles();
         }
 
-        
+        void SetNoAvailableNeighbourTriangles()
+        {
+            var newNotAvailableNeighbourTriangles = new List<TriangleData>();
+            foreach (var triangle in availableTriangles)
+            {
+                if(triangle.GetAvailableNeighbours().Count==0)
+                {
+                    newNotAvailableNeighbourTriangles.Add(triangle);
+                }
+            }
+                    notAvailableNeighbourTriangles.AddRange(newNotAvailableNeighbourTriangles);
+                    newNotAvailableNeighbourTriangles.ForEach(t=>availableTriangles.Remove(t));
+        }
+
+
     }
 
     [System.Serializable]
     public class PuzzlePiece
     {
         public TriangleData[] TriangleDatas { get; }
-        public Vector2[] ContactPoint { get; }
+        public ContactPoint[] ContactPoints { get; }
 
         public PuzzlePiece(TriangleData[] triangleDatas)
         {
@@ -56,25 +124,32 @@ namespace Puzzle2DSystem
                 triangleData.Owner = this;
             }
             TriangleDatas = triangleDatas;
+            var contactPoints = new List<ContactPoint>();
+            foreach (var triangleData in TriangleDatas)
+            {
+                if (!contactPoints.Contains(triangleData.ContactPoint))
+                    contactPoints.Add(triangleData.ContactPoint);
+            }
+            ContactPoints = contactPoints.ToArray();
         }
     }
 
     [System.Serializable]
     public class PuzzleBoard
     {
-        public int RawCount { get; }
-        public int ColumnCount { get; }
         public ContactPoint[] ContactPoints { get; }
+        public int BoardSize { get; }
 
         public PuzzleBoard(int boardSize)
         {
-            RawCount = boardSize * 2;
-            ColumnCount = boardSize * 2;
+            BoardSize = boardSize * 2;
+            var rawCount = BoardSize;
+            var columnCount = BoardSize;
             ContactPoints = new ContactPoint[boardSize*boardSize];
             var index = 0;
-            for (int i = 1; i < RawCount; i+=2)
+            for (int i = 1; i < rawCount; i+=2)
             {
-                for (int j = 1; j < ColumnCount; j+=2)
+                for (int j = 1; j < columnCount; j+=2)
                 {
                     ContactPoints[index] = new ContactPoint(i, j);
                     Debug.Log(index + ": " + ContactPoints[index].Position);
@@ -82,7 +157,6 @@ namespace Puzzle2DSystem
                 }
             }
         }
-
     }
 
 
@@ -92,6 +166,7 @@ namespace Puzzle2DSystem
         public ContactPoint ContactPoint { get; }
         public Vector2 Position { get; }
         public Vector2[] Points { get; }
+        public TriangleData[] Neighbours { get; private set; }
         public PuzzlePiece Owner;
 
         public TriangleData(ContactPoint contactPoint, Vector2[] points)
@@ -108,6 +183,58 @@ namespace Puzzle2DSystem
                 Position /= Points.Length;
             }
         }
+
+        public void SetMyNeighbours(TriangleData[] neighbourCandidates)
+        {
+            List<TriangleData> neighbours = new List<TriangleData>();
+
+            foreach (var candidate in neighbourCandidates)
+            {
+                if (IsMyNeighbour(candidate))
+                    neighbours.Add(candidate);
+            }
+            Neighbours = neighbours.ToArray();
+        }
+
+        public List<TriangleData> GetAvailableNeighbours()
+        {
+            var availableNeighbours = new List<TriangleData>();
+            foreach (var neighbour in Neighbours)
+            {
+                if (neighbour.Owner == null)
+                    availableNeighbours.Add(neighbour);
+            }
+            return availableNeighbours;
+        }
+
+        public TriangleData GetRandomAvailableNeighbour()
+        {
+            TriangleData randomNeighbour = null;
+            var availableNeighbours = GetAvailableNeighbours();
+            if (availableNeighbours.Count > 0)
+                randomNeighbour = availableNeighbours[Random.Range(0, availableNeighbours.Count)];
+                return randomNeighbour;
+        }
+
+
+        bool IsMyNeighbour(TriangleData neighbourCandidate)
+        {
+            int commonPointCount = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Points[i] == neighbourCandidate.Points[j])
+                    {
+                        commonPointCount++;
+                        break;
+                    }
+                }
+            }
+            return commonPointCount == 2;
+        }
+
+        
     }
 
     [System.Serializable]
@@ -133,7 +260,7 @@ namespace Puzzle2DSystem
             Triangles = new TriangleData[8];
             for (int i = 0; i < 8; i++)
             {
-                Triangles[i] = new TriangleData(this,new Vector2[] { Position, clockwisePoints[i], clockwisePoints[i % 8] });
+                Triangles[i] = new TriangleData(this,new Vector2[] { Position, Position+clockwisePoints[i], Position+clockwisePoints[(i+1) % 8] });
             }
         }
 
